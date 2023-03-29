3fcee15f0383fb35320594aace58f5f964413b25
Felipe de Oliveira Renó <felipereno2005@gmail.com>
2023-03-29 12:27:40 -0300

    Criado o Action.php como controller

--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -1,31 +1,19 @@
 <?php
     namespace App\controllers;
+    use Core\controller\Action;
 
-    class IndexController{
-
+    class IndexController extends Action{
         // no contexto de MVC os métodos são chamados de action
         public function index() {
             // supondo que dados estejam vindo do banco
-            $dados = array("Bombom", "Boneco chocolate neymar", "Barra chocolate");
-            $this->render("index", $dados);
+            $this->view->dados = array("Bombom", "Boneco chocolate neymar", "Barra chocolate");
+            $this->render("index");
         }
     
         public function sobre_nos() {
-            $dados = array("Mouse", "Teclado", "Pendrive", "Caixa de Som");
-            $this->render("sobre_nos", $dados);
-        }
-
-        // responsável por renderizar (exibir) a página na tela
-        public function render($view, $dados){
-            // essa função retorna o nome da classe passada como argumento
-            $class = get_class($this);
-
-            $class = str_replace("App\\controllers\\", "", $class);
-            $class = strtolower(str_replace("Controller", "", $class));
-
-            echo($class);
-
-            require_once("../app/views/index/".$view.".phtml");
+            // supondo que dados estejam vindo do banco
+            $this->view->dados = array("Mouse", "Teclado", "Pendrive", "Caixa de Som");
+            $this->render("sobre_nos");
         }
     }
 ?>
---
+++ core/controller/Action.php
@@ -0,0 +1,25 @@
+<?php
+    namespace Core\controller;
+    use stdClass;
+
+    abstract class Action{
+        // criar um atributo vazio
+        protected $view;
+
+        //método cos
+        public function __construct(){
+            $this->view = new stdClass();
+        }
+
+        // responsável por renderizar (exibir) a página na tela
+        protected function render($view){
+            // essa função retorna o nome da classe passada como argumento
+            $class = get_class($this);
+
+            $class = str_replace("App\\controllers\\", "", $class);
+            $class = strtolower(str_replace("Controller", "", $class));
+
+            require_once("../app/views/".$class."/".$view.".phtml");
+        }
+    }
+?>
